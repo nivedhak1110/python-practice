556eac4bf93533aee6c22eddf19c1b3f71f0b617
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-04-23 15:55:55 +0530

    chr_pgm2

---
+++ pattern programs/chr_pgm2.py
@@ -0,0 +1,17 @@
+# Q-28 character pattern program 2 -- to display a word
+""" 
+P
+Py
+Pyt
+Pyth
+Pytho
+Python
+       """
+word=input("enter the word:")       
+#word = "Python"
+x = ""
+for i in word:
+    x += i #concatenation
+    print(x)
+
+        
