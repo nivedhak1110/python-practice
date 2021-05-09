d2e641680b92f0536ea2655528a53b199404f524
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-09 19:25:31 +0530

    Python program to do arithmetical operations

---
+++ Problems/Mustknow_basics/Basicpgms/Q2.py
@@ -0,0 +1,29 @@
+"""Q2 
+Python program to do arithmetical operations
+Addition
+Subtraction
+Multiplication
+Division
+"""
+n1=int(input("enter num1:"))
+n2=int(input("enter num2:"))
+#sum
+sum=n1+n2
+print("sum:",sum)
+#subtraction
+sub=n1-n2
+print("sub:",sub)
+#Multiplication
+mul=n1*n2
+print("mul:",mul)
+#Division
+div=n1/n2
+print("div:",div)
+"""
+Output:
+enter num1:10
+enter num2:5
+sum: 15
+sub: 5
+mul: 50
+div: 2.0"""
