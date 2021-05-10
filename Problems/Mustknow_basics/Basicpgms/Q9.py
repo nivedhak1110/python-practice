9143be43529f83684a11e5c74014fc13c76511ab
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-10 16:57:39 +0530

    Python Program to check if a Number is Positive, Negative

---
+++ Problems/Mustknow_basics/Basicpgms/Q9.py
@@ -0,0 +1,19 @@
+"""
+Q-9 Python Program to check if a Number is Positive, Negative 
+or Zero"""
+num=float(input("enter the number:"))
+if num>0:
+    print("Positive number")
+elif num<0:
+    print("negative number")
+else:
+    print("Zero")
+"""
+Output:
+enter the number:3
+Positive number
+enter the number:0
+Zero
+enter the number:67
+Positive number
+"""
