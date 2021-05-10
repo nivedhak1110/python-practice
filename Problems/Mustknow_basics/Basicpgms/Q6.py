ebbd5cb3e40328b592496d8ade93275e838b2382
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-10 15:30:06 +0530

    Python program to generate a random number

---
+++ Problems/Mustknow_basics/Basicpgms/Q6.py
@@ -0,0 +1,20 @@
+"""
+Q6
+Python program to generate a random number
+In Python, you can generate a random integer, 
+doubles, longs etc . in various ranges by importing a 
+"random" class.
+
+Syntax:
+import random  
+random.randint(a,b)  
+First you have to import the random module and then apply the 
+"""
+import random
+print("random int between 100-200:",random.randint(100,200))
+print("random int between 100-200:",random.randrange(100,800))
+"""
+Output:
+random int between 100-200: 195
+random int between 100-200: 259
+"""
