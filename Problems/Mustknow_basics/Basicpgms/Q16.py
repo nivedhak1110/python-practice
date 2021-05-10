5def256b5f0345ec824591faa1f376e816fee8e5
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-10 22:38:20 +0530

    Create Q16.py

---
+++ Problems/Mustknow_basics/Basicpgms/Q16.py
@@ -0,0 +1,10 @@
+"""
+Q-16
+Python Program to Check Armstrong Number
+Armstrong number:
+
+A number is called Armstrong number if it is equal to the sum 
+of the cubes of its own digits.
+For example: 153 is an Armstrong number
+since 153 = 1*1*1 + 5*5*5 + 3*3*3
+"""
