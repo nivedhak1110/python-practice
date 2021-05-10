8cc93cd980f71a1813cf06990c64273bb7958534
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-10 17:09:25 +0530

    Python Program to Check if a Number is Odd or Even

---
+++ Problems/Mustknow_basics/Basicpgms/Q10.py
@@ -0,0 +1,15 @@
+"""
+Q-10 Python Program to Check if a Number is Odd or Even
+"""
+num=int(input("enter a number:"))
+if num%2==0:
+    print("even")
+else:
+    print("odd")
+"""
+Output:
+enter a number:9
+odd
+enter a number:26
+even
+"""
