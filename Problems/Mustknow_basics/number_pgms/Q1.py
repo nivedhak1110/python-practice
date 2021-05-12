d64e73b738743df085e8eec44fdd03af797de622
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-12 14:13:25 +0530

    Create Q1.py

---
+++ Problems/Mustknow_basics/number_pgms/Q1.py
@@ -0,0 +1,7 @@
+"""
+Python program to check if the given number is a Disarium Number
+A number is said to be the Disarium number when the sum of its digit raised to the power of their respective positions becomes equal to the number itself.
+
+For example, 175 is a Disarium number as follows:
+
+11+ 72 + 53 = 1+ 49 + 125 = 175"""
