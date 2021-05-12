c8ef3983e7954c4effb67f7b8fbc7eafe0eca14f
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-12 21:12:35 +0530

    Update Q3.py

--- Problems/Mustknow_basics/number_pgms/Q3.py
+++ Problems/Mustknow_basics/number_pgms/Q3.py
@@ -1,27 +1,20 @@
 """
-Q-3
-Python program to check if the given number is Happy Number
-A number is said to be happy if it yields 1 when replaced by 
-the sum of squares of its digits repeatedly. If this process 
-results in an endless cycle of numbers containing 4, then 
-the number will be an unhappy number.
-
-Let's understand by an example:
-
-Number = 32
-32+ 22 = 13
-12 + 32 = 10
-12 + 02 = 1
-
-
-In this example, we split 32 to get the sum of squares of 
-its digits which forms another number (13), we replace 32 by 
-13 to continue this cycle until result 1. We found 32 a 
-happy number.
-
-If the above cycle for any number results in 1 then that 
-number will be a Happy number otherwise that will be an 
-unhappy number resulting 4, 16, 37, 58, 89, 145, 42, 20.....
-
-Some Happy numbers are 7, 28, 100, 320, etc.
+Q-3 Python program to check if the given number is Happy Number
++++++
+Happy Number:
+A number is called happy if it leads to 1 after a sequence 
+of steps wherein each step number is replaced by the sum of 
+squares of its digit that is if we start with Happy Number
+and keep replacing it with digits square sum, we reach 1. 
++++++
+Input: n = 19
+Output: True
+19 is Happy Number,
+1^2 + 9^2 = 82
+8^2 + 2^2 = 68
+6^2 + 8^2 = 100
+1^2 + 0^2 + 0^2 = 1
+As we reached to 1, 19 is a Happy Number.
 """
+num=int(input("enter a number:"))
+temp=num
