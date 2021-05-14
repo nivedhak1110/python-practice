d489ff804076e027a0c98446b838d8e5dab6b54c
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-14 17:12:15 +0530

    Python program to check if the given number is a  Harshad Number

---
+++ Problems/Mustknow_basics/number_pgms/Q5.py
@@ -0,0 +1,19 @@
+"""
+Q-5
+Python program to determine whether the given number is a 
+Harshad Number.
+If a number is divisible by the sum of its digits, then it will 
+be known as a Harshad Number.
+"""
+num=int(input("Enter a number:"))
+temp=num
+sum=0
+while num>0:
+    rem=num%10
+    sum=sum+rem
+    num=num//10
+print(sum)
+if temp%sum==0:
+    print("{temp} is a Harshad number".format(temp=temp))
+else:
+    print("{temp} is Not a harshad number".format(temp=temp))
