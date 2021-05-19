25ead75449f86966b32082131637a656430748a0
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-19 21:41:43 +0530

    convert list to string in Python

---
+++ Problems/Mustknow_basics/string_basicpgms/Q4.py
@@ -0,0 +1,13 @@
+"""
+Q-4 convert list to string in Python
+
+"""
+words=["Iam","learning","python","programming"]
+str=""
+for word in words:
+    str=str+word+" "
+print(str)
+"""
+Output:
+Iam learning python programming 
+"""
