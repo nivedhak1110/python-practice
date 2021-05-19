d2a2555bfc861d6ac2b03c9c931a714eb581a358
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-19 18:03:46 +0530

    Create Q1.py

---
+++ Problems/Mustknow_basics/string_basicpgms/Q1.py
@@ -0,0 +1,6 @@
+"""
+Python Program to Sort Words in Alphabetic Order
+Sorting:
+
+Sorting is a process of arrangement. It arranges data systematically in a particular format. It follows some algorithm to sort data.
+"""
