6fc9f2c7e2a78df99b08ec1ad0222e24c75baa0b
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-19 14:53:42 +0530

    Python program to print the elements of an array in reverse

---
+++ Problems/Mustknow_basics/Array_basicpgms/Q5.py
@@ -0,0 +1,18 @@
+"""
+Q-5
+Python program to print the elements of an array in reverse 
+order
+"""
+array=[1,2,3,4,5,6,7]
+for i in range(len(array)-1,-1,-1):
+    print(array[i])
+"""
+Output:
+7
+6
+5
+4
+3
+2
+1
+"""
