7cddc8137f85a1ebe6a1125297fc645c735cd25b
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-19 16:31:00 +0530

    Python program to print the number of elements present in an array

---
+++ Problems/Mustknow_basics/Array_basicpgms/Q10.py
@@ -0,0 +1,12 @@
+"""
+Q-10
+Python program to print the number of elements present in an
+array
+"""
+array=[1,2,3,4,5,6,7]
+length=len(array)
+print("there are {length} elements in the array".format(length=length))
+"""
+Output:
+there are 7 elements in the array
+"""
