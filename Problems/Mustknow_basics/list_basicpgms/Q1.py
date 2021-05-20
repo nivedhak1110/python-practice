3389632570833729e7764b4b88fb479a0c33b835
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-20 18:48:05 +0530

    Create Q1.py

---
+++ Problems/Mustknow_basics/list_basicpgms/Q1.py
@@ -0,0 +1,7 @@
+"""
+Q-1
+How to append element in the list
+*append(elmt) - It appends the value at the end of the list.
+*insert(index, elmt) - It inserts the value at the specified index position.
+*extends(iterable) - It extends the list by adding the iterable object.
+"""
