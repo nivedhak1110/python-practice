442d3f1ca81af48c39b0782256a83f3a7ab70cdf
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-22 20:14:43 +0530

    selection Sort in Python

--- Problems/Mustknow_basics/Search_sortingpgms/Q3.py
+++ Problems/Mustknow_basics/Search_sortingpgms/Q3.py
@@ -1,5 +1,9 @@
 """
 Q-3 selection Sort in Python
+The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning. The algorithm maintains two subarrays in a given array.
+
+1) The subarray which is already sorted.
+2) Remaining subarray which is unsorted.
 
 """
 A=[10,3,1,32,45,667,21,42,111,285]
