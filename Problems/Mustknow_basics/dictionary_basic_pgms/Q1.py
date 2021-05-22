dacdd0f2f4f8e6e65b25cb4247317ec6f0d13df8
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-05-22 14:37:40 +0530

    create a dictionary in python

--- Problems/Mustknow_basics/dictionary_basic_pgms/Q1.py
+++ Problems/Mustknow_basics/dictionary_basic_pgms/Q1.py
@@ -1,6 +1,7 @@
 """
 Q-1
 create a dictionary
+-->direct method ,using dict()method
 """
 #method1
 student={
@@ -10,9 +11,16 @@ student={
 }
 print(type(student))
 print(student)
+# method 2 using the dict() method  
+teacher=dict({"ramya":"CSE","primya":"ECE","suresh":"CSE"})
+print(teacher)
+# method 3 with each item in list as a key value pair
+teacher_age=dict([("suresh",35),("ramya",30),("primya",29)])
+print(teacher_age)
 """
 Output:
 <class 'dict'>
 {'ram': 83, 'raj': 56, 'ravi': 90}
-
+{'ramya': 'CSE', 'primya': 'ECE', 'suresh': 'CSE'}
+{'suresh': 35, 'ramya': 30, 'primya': 29}
 """
