178476c0087c527d86b821b9b988dc032256d748
Nivedha <62648927+nivedhak1110@users.noreply.github.com>
2021-06-08 07:52:46 +0530

    selection sort

--- Problems/Mustknow_basics/Search_sortingpgms/Q3.py
+++ Problems/Mustknow_basics/Search_sortingpgms/Q3.py
@@ -5,10 +5,11 @@ The selection sort algorithm sorts an array by repeatedly finding the minimum el
 2) Remaining subarray which is unsorted.
 """
 def selection_sort(array):
-    min_index=0
+    
     for i in range(len(array)):
+        min_index=i
         for j in range(i+1,len(array)):
-            if array[j]<array[i]:
+            if array[min_index]>array[j]:
                 min_index=j
         array[i],array[min_index]=array[min_index],array[i]#swap the elements in one line without using temporary variable
     return array            
@@ -19,8 +20,9 @@ if __name__=='__main__':
     print("Initial unsorted array:",array)
     sorted_array=selection_sort(array)
     print("array sorted using selection sort:",sorted_array)
+
 """
 Output:
 Initial unsorted array: [111, 22, 12, 34, 43, 749, 322, 21, 10, 3]
-array sorted using selection sort: [3, 10, 22, 12, 34, 111, 43, 322, 749, 21]
+array sorted using selection sort: [3, 10, 12, 21, 22, 34, 43, 111, 322, 749]
 """
